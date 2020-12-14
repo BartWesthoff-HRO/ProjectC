3965ae1a46733321cdc17c09caa801313b4b1063
BartWesthoff <0991807@hr.nl>
2020-12-14 14:22:32 +0100

    api key goed en dropdown

--- Controllers/CampaignsController.cs
+++ Controllers/CampaignsController.cs
@@ -1,5 +1,6 @@
 ﻿using MailChimp.Net;
 using MailChimp.Net.Core;
+using ProjectC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,13 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace ProjectC.Controllers
 {
     public class CampaingsController : Controller
     {
 
-        private static MailChimpManager Manager = new MailChimpManager("e8453349ccde9693bf86d9760787356f-us7");
+        private static MailChimpManager Manager = new MailChimpManager("c21b2291eb1b26c7ea4b1a86908dcc78-us2");
 
         public async Task<ActionResult> Drafts()
         {
@@ -36,24 +38,22 @@ namespace ProjectC.Controllers
         public async Task<ActionResult> Send(string id)
         {
 
-            //await Manager.Campaigns.SendAsync(id);
+            await Manager.Campaigns.SendAsync(id);
             return RedirectToAction("index");
         }
 
-        public async Task<ActionResult> Sent()
+        public async Task<ActionResult> Sent(string id = "b8ddf89ff8")
         {
             var options = new CampaignRequest
             {
-                ListId = "14d2abf97d",
+                ListId = id,
                 Status = CampaignStatus.Sent,
                 SortOrder = CampaignSortOrder.DESC,
                 Limit = 10
             };
+            ViewBag.Teachers = await Manager.Lists.GetAllAsync();
 
-            ViewBag.ListId = "14d2abf97d";
-            ViewBag.ListName = "Project C";
-
-
+            //14d2abf97d
             var model = await Manager.Campaigns.GetAllAsync(options);
             return View(model);
 
