ae0f389a5cb7b9de37ca402835618528385fdf70
Nathan <nsamijo@gmail.com>
2021-01-13 14:50:35 +0100

    Updated object
    Updated and added a new model to display labels of a person. is not working yet. Application is working

---
+++ Models/IndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+using System.ComponentModel;
+using System.Data.Entity;
+using ProjectC.Models;
+
+
+namespace ProjectC.Models
+{
+    public class IndexViewModel
+    {
+        public List<ContactPerson> people { get; set; }
+        public List<label> labels { get; set; }
+        public List<Kenmerk> kenmerken { get; set; }
+        public List<klant> klanten { get; set; }
+    }
+}
